f786a894e48c883e83cb82c62c4185e9cb5811fa
hin6150 <hin6150@gmail.com>
2024-08-11 11:33:32 +0900

    feat: Validation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,34 +1,35 @@
 'use client'
 
-import FormButton from '@/components/form-btn'
-import FormInput from '@/components/form-input'
+import FormButton from '@/components/button'
+import FormInput from '@/components/input'
 import SocialLogin from '@/components/social-login'
+import { PASSWORD_MIN_LENGTH } from '@/lib/constants'
 import { useFormState } from 'react-dom'
-import { handleForm } from './actions'
+import { logIn } from './actions'
 
 export default function LogIn() {
-  const [state, action] = useFormState(handleForm, null)
-
+  const [state, dispatch] = useFormState(logIn, null)
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form action={action} className="flex flex-col gap-3">
+      <form action={dispatch} className="flex flex-col gap-3">
         <FormInput
           name="email"
           type="email"
           placeholder="Email"
           required
-          errors={[]}
+          errors={state?.fieldErrors.email}
         />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
-          errors={state?.errors ?? []}
+          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.fieldErrors.password}
         />
         <FormButton text="Log in" />
       </form>
